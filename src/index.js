4f1a228571bd609313db6eaa6e4382ef605c70aa
Steven Rosario <strosariogarcia@gmail.com>
2022-09-07 14:54:47 -0400

    Extra Deliverables

--- src/index.js
+++ src/index.js
@@ -3,45 +3,46 @@ let ramen_menu = document.getElementById("ramen-menu")
 let ramen_display = document.getElementById("ramen-detail")
 let rating = document.getElementById("rating-display")
 let comment = document.getElementById("comment-display")
-let form = document.getElementById("new-ramen")
+let form = document.querySelector("#new-ramen")
+let update = document.getElementById("edit-ramen")
 
-form.addEventListener("submit", (event) => {
-        event.preventDefault();
-        fetch("http://localhost:3000/ramens", {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                name: event.target.name.value,
-                restaurant: event.target.restaurant.value,
-                image: event.target.image.value
-            })
-        })
-    })
-
-
-// uses fetch on the JSON file and returns a promise
 const fetchRamen = () => {
    return fetch("http://localhost:3000/ramens")
     .then(res => res.json())
 }
 
-// creates the ramen in the div 
-const createRamen = (ramen) => {
+const postRamen = (ramen) => {
     let ramens = document.createElement("li")
     
     let ramen_img = document.createElement("img")
     ramen_img.src = ramen.image
 
-    ramen_menu.appendChild(ramen_img);
-
     ramen_img.addEventListener("click", (event) => {
-        document.querySelector(".detail-image").src = ramen.image
-        document.querySelector(".name").textContent = ramen.name
-        document.querySelector(".restaurant").textContent = ramen.restaurant
-        rating.textContent = ramen.rating
-        comment.textContent = ramen.comment
+        ramenDetail(ramen)
+    })
+    ramen_menu.appendChild(ramen_img)
+}
+
+const ramenDetail = (ramen) => {
+    document.querySelector(".detail-image").src = ramen.image
+    document.querySelector(".name").textContent = ramen.name
+    document.querySelector(".restaurant").textContent = ramen.restaurant
+    rating.textContent = ramen.rating
+    comment.textContent = ramen.comment 
+
+    update.addEventListener("submit", (e) => {
+        e.preventDefault();
+        fetch(`http://localhost:3000/ramens/${ramenObj.id}`, {
+            method: "PATCH",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                rating: e.target.rating.value,
+                comment: e.target.querySelector("#new-comment").value,
+            }),
+        })
     })
 }
 
@@ -49,4 +50,22 @@ fetchRamen().then(data => {
     data.forEach(ramen => {
         postRamen(ramen)
     })
+});
+
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    fetch("http://localhost:3000/ramens", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            name: e.target.name.value,
+            restaurant: e.target.restaurant.value,
+            image: e.target.image.value,
+            rating: e.target.rating.value,
+            comment: e.target.newComment.value,
+        }),
+    });
 });
